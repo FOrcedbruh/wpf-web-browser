2dce3b502d2e2bcb2f1fdf9f0d606fb85963b5f3
Ilya <ilya925512@gmail.com>
2024-11-14 21:28:33 +0300

    add tabs

--- WebBrowser/MainWindow.xaml.cs
+++ WebBrowser/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Wpf;
 
 namespace WebBrowser
 {
@@ -20,29 +21,85 @@ namespace WebBrowser
         public MainWindow()
         {
             InitializeComponent();
+            CreateNewTab("https://www.google.com"); 
         }
 
+       
+        private void CreateNewTab(string url)
+        {
+            
+            var tabItem = new TabItem();
+            var headerPanel = new StackPanel { Orientation = Orientation.Horizontal };
+
+
+            var headerText = new TextBlock { Text = "New Tab", Margin = new Thickness(0, 0, 5, 0) };
+            headerPanel.Children.Add(headerText);
+
+            
+            var closeButton = new Button { Content = "X", Width = 20, Height = 20, Padding = new Thickness(0) };
+            closeButton.Click += (s, e) => CloseTab(tabItem); // Закрытие вкладки при нажатии на "X"
+            headerPanel.Children.Add(closeButton);
+
+            tabItem.Header = headerPanel;
+
+            
+            var browser = new WebView2
+            {
+                Source = new Uri(url)
+            };
+
+            
+            tabItem.Content = browser;
+
+            
+            TabControl.Items.Add(tabItem);
+            TabControl.SelectedItem = tabItem; 
+        }
+
+        
+        private void CloseTab(TabItem tabItem)
+        {
+            if (TabControl.Items.Count > 1) 
+            {
+                TabControl.Items.Remove(tabItem);
+            }
+            else
+            {
+                MessageBox.Show("Нельзя закрыть последнюю вкладку.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            if (Browser.CanGoBack)
-            {
-                Browser.GoBack();
-            }
+            GetCurrentBrowser()?.GoBack();
         }
 
+       
         private void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
-            if (Browser.CanGoForward)
-            {
-                Browser.GoForward();
-            }
+            GetCurrentBrowser()?.GoForward();
         }
 
+        
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetCurrentBrowser()?.Reload();
+        }
+
+       
         private void GoButton_Click(object sender, RoutedEventArgs e)
         {
             NavigateToUrlOrSearch();
         }
 
+        
+        private void NewTabButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewTab("https://www.google.com");
+        }
+
+        
         private void UrlTextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -51,32 +108,44 @@ namespace WebBrowser
             }
         }
 
-        private void RefreshButton_Click(object sender, RoutedEventArgs e)
-        {
-            Browser.Reload();
-        }
+        
         private void NavigateToUrlOrSearch()
         {
             string input = UrlTextBox.Text.Trim();
 
             if (IsValidUrl(input))
             {
-               
-                Browser.Source = new Uri(input);
+                GetCurrentBrowser().Source = new Uri(input);
             }
             else
             {
-                
                 string searchQuery = "https://www.google.com/search?q=" + Uri.EscapeDataString(input);
-                Browser.Source = new Uri(searchQuery);
+                GetCurrentBrowser().Source = new Uri(searchQuery);
             }
+        }
+
+        
+        private WebView2 GetCurrentBrowser()
+        {
+            if (TabControl.SelectedItem is TabItem selectedTab && selectedTab.Content is WebView2 browser)
+            {
+                return browser;
+            }
+            return null;
         }
 
         
         private bool IsValidUrl(string url)
         {
             return Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var browser = GetCurrentBrowser();
+            UrlTextBox.Text = browser?.Source?.ToString() ?? "https://www.google.com";
         }
     }
 }
